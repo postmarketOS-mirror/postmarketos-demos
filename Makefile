193462a3ee6b8be18b121dbeb1d07780bd25778a
HenriDellal <anridellal@gmail.com>
2020-05-14 21:58:23 +0300

    Adapt UI to low-res screens (MR 1)

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 postmarketos-demos: main.c
 	$(CC) `pkg-config --cflags gtk+-3.0` -o postmarketos-demos \
-		main.c `pkg-config --libs gtk+-3.0` -Werror
+		main.c `pkg-config --libs gtk+-wayland-3.0 gtk+-3.0` -Werror \
+		-Wno-error=deprecated-declarations
--- main.c
+++ main.c
@@ -15,22 +15,61 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <gdk/gdk.h>
+#include <gdk/gdkwayland.h>
+#include <gdk/gdkx.h>
 #include <gtk/gtk.h>
 #include <stdlib.h>
+
+#define DEFAULT_WINDOW_WIDTH 512
+#define DEFAULT_WINDOW_HEIGHT 70 * (8 + 1)
 
 static void onclick(GtkWidget *widget, gpointer command)
 {
 	system((const char *)command);
 }
 
+void adjust_window_size(GtkWindow *window) {
+	int width = DEFAULT_WINDOW_WIDTH;
+	int height = DEFAULT_WINDOW_HEIGHT;
+	GdkDisplay *display = gdk_display_get_default();
+#ifdef GDK_WINDOWING_WAYLAND
+	if (GDK_IS_WAYLAND_DISPLAY(display)) {
+		GdkRectangle geometry;
+		GdkMonitor *monitor = gdk_display_get_monitor(display, 0);
+		gdk_monitor_get_geometry(monitor, &geometry);
+		width = (width < geometry.width) ? width : geometry.width;
+		height = (height < geometry.height) ? height : geometry.height;
+	}
+	else
+#endif
+#ifdef GDK_WINDOWING_X11
+	if (GDK_IS_X11_DISPLAY(display)) {
+		GdkScreen *screen = gdk_screen_get_default();
+		int max_width = gdk_screen_get_width(screen);
+		int max_height = gdk_screen_get_height(screen);
+		printf("%d %d", max_width, max_height);
+		if (width > max_width)
+			width = max_width;
+
+		if (height > max_height)
+			height = max_height;
+	}
+	else
+#endif
+		g_error("Unsupported GDK backend");
+
+	gtk_window_resize(window, width, height);
+}
+
 static void activate(GtkApplication *app, gpointer user_data)
 {
 	GtkWidget *window = gtk_application_window_new(app);
+	GtkWidget *scr_window = gtk_scrolled_window_new(NULL, NULL);
 	gtk_window_set_title(GTK_WINDOW(window), "postmarketOS demos");
-
-	GtkWidget *button_box = gtk_button_box_new(
-		GTK_ORIENTATION_VERTICAL);
-	gtk_container_add(GTK_CONTAINER(window), button_box);
+	gtk_container_add(GTK_CONTAINER(window), scr_window);
+	GtkWidget *list_box = gtk_list_box_new();
+	gtk_container_add(GTK_CONTAINER(scr_window), list_box);
 
 	const char *programs[] = {
 		"GTK3 Demo",
@@ -59,8 +98,9 @@ static void activate(GtkApplication *app, gpointer user_data)
 		gtk_widget_set_size_request(button, 200, 70);
 		g_signal_connect(button, "clicked", G_CALLBACK(onclick),
 			(void *)command);
-		gtk_container_add(GTK_CONTAINER(button_box), button);
+		gtk_list_box_insert(GTK_LIST_BOX(list_box), button, -1);
 	}
+	adjust_window_size(GTK_WINDOW(window));
 	gtk_widget_show_all(window);
 }
 
